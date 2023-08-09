bda4cb132eaba85405924cfacf0cc83213e53f03
Oluwasetemi Ojo <setemiojo@gmail.com>
2023-08-09 13:16:55 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+import './style.css';
+
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+//
+
+button.addEventListener('click', createTodo);
+
+input.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    createTodo();
+  }
+});
+
+function createTodo(event) {
+
+    let input = button.previousElementSibling;
+    let divTodo = button.nextElementSibling;
+    let ul = divTodo.firstElementChild;
+    // console.log(ul);
+    const todo = input.value;
+    // create li
+
+    let li = document.createElement('li');
+    // set textContent
+    li.textContent = todo.trim();
+
+    // single click on the line mean todo is done
+    li.onclick = (event) => {
+      event.target.classList.toggle('completed');
+    };
+
+    // double click on the line mean edit todo
+
+    // add a delete button
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    // add onclick event to button
+    deleteButton.onclick = (event) => {
+      // remove li
+      let deleteTodoConfirmed = confirm('todo removed successfully !');
+      if (deleteTodoConfirmed) {
+        event.target.parentElement.parentElement.remove();
+      }
+      // alert latter
+    };
+
+    let span = document.createElement('span');
+    span.append(deleteButton);
+    li.append(span);
+    // append li to ul
+    ul.append(li);
+
+    // clear input
+    input.value = '';
+}
