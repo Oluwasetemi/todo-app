2eda419e0fe1fc5cadb2bfdbc25adaced521e671
ozo-vehe <johnnie.vehe@gmail.com>
2023-08-10 21:37:57 +0100

    added onload event to get todos, and display them.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import './style.css';
 
 const button = document.querySelector('button');
 const input = document.querySelector('input');
-//
 
 button.addEventListener('click', createTodo);
 
@@ -12,20 +11,69 @@ input.addEventListener('keydown', (event) => {
     createTodo();
   }
 });
+// Get all the todos from the local storage
+const storage = localStorage.getItem("todos");
+console.log(storage);
 
-function createTodo(event) {
+// Create an onload event to load the todos from the storage
+window.onload = (e) => {
+  if (storage) {
+    // Split the stored todos into the storageTodoArray variable
+    const storageTodoArray = storage.split(",");
+    // Create a forEach loop to loop through the storageTodoArray and create the todos to be displayed
+    storageTodoArray.forEach((todo) => {
+      // Call the createTodo function to create the todo from the stored todos
+      createTodo(e, todo);
+    });
+  }
+}
+
+// Added a savedTodo parameter to the createTodo function and set it to null as default
+function createTodo(event, savedTodo = null) {
   let input = button.previousElementSibling;
   let divTodo = button.nextElementSibling;
   let ul = divTodo.firstElementChild;
   // console.log(ul);
-  const todo = input.value.trim();
+
+  // Create a variable to hold each todo
+  let todo;
+
+  // Check if the function is called from the onload event with a saved todo
+  if (savedTodo) {
+    // If there is a saved todo, use it
+    todo = savedTodo;
+    // else use the input value
+  } else {
+    // If there is no saved todo, use the input value
+    todo = input.value.trim();
+    console.log(todo);
+    // check if the todo is empty
+    if (todo == '') {
+      // If the todo is empty, return from the function
+      return;
+    }
+
+    // Get current todos from the local storage
+    const storage = localStorage.getItem("todos");
+    // Check if there are todos in the local storage    
+    if (!storage) {
+      // If there are no todos in the local storage, save the todo
+      localStorage.setItem("todos", todo);
+    }
+    // If there are todos in the local storage, add the new todo to the existing todos
+    else {
+      localStorage.setItem("todos", [storage, todo]);
+    }
+    // Console log the saved todo
+    console.log(localStorage.getItem("todos"));
+  }
 
   // create li
   let li = document.createElement('li');
   // set textContent
-  if (todo == '') {
-    return;
-  }
+  // if (todo == '') {
+  //   return;
+  // }
 
   li.textContent = todo;
 
